299d20eca3b92274f09b5546ca167778ba8e564e
wenhao <wb-ywh474663@antfin.com>
2018-12-05 16:52:23 +0800

    栈相关题目

--- src/main/java/algorithm/StackRelated/DogAndCat.java
+++ src/main/java/algorithm/StackRelated/DogAndCat.java
@@ -3,10 +3,137 @@
  */
 package algorithm.StackRelated;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * 猫狗队列的实现
+ * 要求：可以直接查猫
+ * 也可以直接查狗
+ * 也可以按照插入顺序依次输出猫或者狗
  * @author wb-ywh474663
  * @version $Id: DogAndCat.java, v 0.1 2018年12月04日 19:40 wb-ywh474663 Exp $
  */
 public class DogAndCat {
+    /**
+     * 设计思路，还是猫，狗队列两个，但是需要保存时间戳，用于顺序输出
+     * 时间戳其实有点问题，因为可能连续add 时间戳是一样的，那就尴尬了
+     *
+     * 所以我觉得另外一个思路可以维护一个链表结构，缺点是维护成本高一点
+     * cat或者 dog remove 的时候该链表结构也要维护
+     */
+
+    public abstract static class Base{
+
+        long timeStamp;
+
+        public Base(){
+            timeStamp = System.currentTimeMillis();
+            try{
+                Thread.sleep(1);
+            }catch (InterruptedException exp){
+
+            }
+
+        }
+
+    }
+
+    public static class Cat extends Base{
+        String type = "Cat";
+        String name;
+        public Cat(String name){
+            super();
+            this.name = name;
+        }
+    }
+
+    public static class Dog extends Base{
+        String type = "Dog";
+        String name;
+        public Dog(String name){
+            super();
+            this.name = name;
+        }
+    }
+
+    private LinkedList<Cat> catQueue;
+    private LinkedList<Dog> dogQueue;
+
+    public DogAndCat(){
+        catQueue = new LinkedList<Cat>();
+        dogQueue = new LinkedList<Dog>();
+    }
+
+    public void add(Cat cat){
+        catQueue.add(cat);
+    }
+    public void add(Dog dog){
+        dogQueue.add(dog);
+    }
+
+    public void pullCat(){
+        while(!catQueue.isEmpty()){
+            System.out.println(catQueue.remove().name);
+        }
+    }
+
+    public void pullDog(){
+        while(!dogQueue.isEmpty()){
+            System.out.println(dogQueue.remove().name);
+        }
+    }
+
+    public boolean isEmpty(){
+        return dogQueue.isEmpty() && catQueue.isEmpty();
+    }
+
+    public boolean isCatEmpty(){
+        return catQueue.isEmpty();
+    }
+
+    public boolean isDogEmpty(){
+        return dogQueue.isEmpty();
+    }
+
+    //按照先后顺序来依次弹出
+    public void pullAll(){
+        while(!isEmpty()){
+            if(catQueue.isEmpty()){
+                if(dogQueue.isEmpty()){
+                    return;
+                }else{
+                    while(!dogQueue.isEmpty()){
+                        System.out.println(dogQueue.remove().name);
+                    }
+                }
+            }else{
+                if(dogQueue.isEmpty()){
+                    while(!catQueue.isEmpty()){
+                        System.out.println(catQueue.remove().name);
+                    }
+                }else{
+                    if(catQueue.getFirst().timeStamp <= dogQueue.getFirst().timeStamp){
+                        System.out.println(catQueue.remove().name);
+                    }else{
+                        System.out.println(dogQueue.remove().name);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        DogAndCat dogAndCatQueue = new DogAndCat();
+
+        dogAndCatQueue.add(new Cat("cat1"));
+        dogAndCatQueue.add(new Cat("cat2"));
+        dogAndCatQueue.add(new Dog("dog1"));
+        dogAndCatQueue.add(new Cat("cat3"));
+        dogAndCatQueue.add(new Dog("dog2"));
+
+        dogAndCatQueue.pullAll();
+    }
+
 }
--- src/main/java/algorithm/StackRelated/StackSort.java
+++ src/main/java/algorithm/StackRelated/StackSort.java
@@ -3,10 +3,73 @@
  */
 package algorithm.StackRelated;
 
+import java.util.Stack;
+
 /**
  * 栈排序，限制：只能使用一个额外的栈，其他数据结构不能用
  * @author wb-ywh474663
  * @version $Id: StackSort.java, v 0.1 2018年12月04日 19:41 wb-ywh474663 Exp $
  */
 public class StackSort {
+
+
+    public static void sort(Stack<Integer> stack){
+            Stack<Integer> tempStack = new Stack<Integer>();
+            while(!stack.isEmpty()){
+                //为空时第一个元素直接入栈
+                if(tempStack.isEmpty()){
+                    tempStack.add(stack.pop());
+                }else{
+                    /**
+                     *  思路： 目标栈要从小到大那么临时栈就要从大到小，这样最后
+                     *  就可以重新压回的时候栈变为从小到大
+                     */
+                    if(tempStack.peek() >= stack.peek()){
+                        tempStack.add(stack.pop());
+                    }else{
+                        Integer temp = stack.pop();
+                        while((!tempStack.isEmpty()) && tempStack.peek() < temp){
+                            stack.push(tempStack.pop());
+                        }
+                        tempStack.push(temp);
+                    }
+                }
+            }
+            while(!tempStack.isEmpty()){
+                stack.push(tempStack.pop());
+            }
+    }
+
+    public static void printStack(Stack<Integer> stack){
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop());
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Stack<Integer> test = new Stack<Integer>();
+        test.add(1);
+        test.add(9);
+        test.add(6);
+        test.add(7);
+        test.add(11);
+        test.add(10);
+        test.add(9);
+        test.add(2);
+        printStack(test);
+        test.add(1);
+        test.add(9);
+        test.add(6);
+        test.add(7);
+        test.add(11);
+        test.add(10);
+        test.add(9);
+        test.add(2);
+        sort(test);
+        printStack(test);
+
+    }
+
 }
