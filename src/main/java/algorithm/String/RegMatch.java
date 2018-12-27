ae6405bef8ef37943e4d82b54c55a4bdd47843c3
wenhao <wb-ywh474663@antfin.com>
2018-12-27 20:38:07 +0800

    完成面试题19

--- src/main/java/algorithm/String/RegMatch.java
+++ src/main/java/algorithm/String/RegMatch.java
@@ -3,66 +3,72 @@
  */
 package algorithm.String;
 
+import java.util.Arrays;
+
 /**
  * 正则表达式匹配
  * 实现一个函数用来匹配包含“.”和"*"的正则表达式，“.”表示任意一个字符，“*”表示它
  * 前面的字符可以出现任意次(含0次)，此外，匹配是整个字符串，不考虑匹配局部
  *
  * 这个问题解决思路上没有特别之处，关键是对比是能够把各种情况都考虑全
+ *
+ *
+ * 1227 这个问题编了两天，似乎想复杂了，正则匹配肯定比这个还复杂，不也一样
+ *
+ * 思路上的一个漏洞：其实多种情况，利用递归穷举就可以了，不符合就返回false而已，不用
+ * 每种情况都if else判断情况，只会把自己搞乱
  * @author wb-ywh474663
  * @version $Id: RegMatch.java, v 0.1 2018年12月25日 10:20 wb-ywh474663 Exp $
  */
 public class RegMatch {
-
-        public static boolean regMatch(String string ,String matcher){
-            if(string == null || matcher == null){
+        //考虑性能，应该用char[],String有点过
+        public static boolean regMatch(String[] target,int targetIndex, String[] matcher,int matcherIndex){
+            if(target == null || matcher == null){
                 return false;
             }
-            if(string.length() == 0 || matcher.length() == 0){
+            /**
+             * 正则表达式先到头，那么匹配失败
+             */
+            if(targetIndex < target.length && matcherIndex == matcher.length){
                 return false;
             }
-            String[] target = string.split("");
-            String[] matcherReg = string.split("");
-            int targetLength = target.length;
-            int matcherRegLength = matcherReg.length;
-            int targetStart = 0;
-            int matcherRegStart = 0;
-            int preMatcherPosition = 0;
-
-            while(targetStart <=targetLength-1 && matcherRegStart<=matcherRegLength-1 ){
-                if(target[targetStart].equals(matcherReg[matcherRegStart])){
-                     targetStart++;
-                     matcherRegStart++;
-                }else{
-                    if(matcherReg[matcherRegStart].equals(".")){
-                        targetStart++;
-                        matcherRegStart++;
-                    }else if(matcherReg[matcherRegStart].equals("*")){
-                        preMatcherPosition = targetStart-1;
-                        if(preMatcherPosition<0) {
-                            return false;
-                        }else{
-                            /**这里的逻辑会相对复杂一点,要判断什么时候可以matcherRegStart
-                             * 位置
-                             * */
-
-
-                        }
-                    }
+            /**
+             * 全都匹配到头，那么匹配成功
+             */
+            if(targetIndex == target.length && matcherIndex == matcher.length){
+                return true;
+            }
+            if((matcherIndex+1)<matcher.length &&matcher[matcherIndex+1].equals("*")){
+                /**
+                 * 分三种情况, "."的情况也要考虑到
+                 * 忽略
+                 * 移动一个
+                 * matcher 不用，target移动一格
+                 * 同时判断，有一个是true就是匹配的
+                 */
+                if(targetIndex<target.length&&(target[targetIndex].equals(matcher[matcherIndex]) || matcher[matcherIndex].equals("."))){
+                    return regMatch(target,targetIndex+1, matcher,matcherIndex+2)||
+                            regMatch(target,targetIndex+1, matcher,matcherIndex);
+                }else {
+                    return regMatch(target,targetIndex, matcher,matcherIndex+2);
                 }
-
-
+            }else{
+                if(targetIndex<target.length&& target[targetIndex].equals(matcher[matcherIndex])){
+                    return regMatch(target,targetIndex+1, matcher,matcherIndex+1);
+                }else {
+                    return false;
+                }
             }
+
 
 
         }
 
         public static void main(String[] args){
-            int target = 2;
-            int pre = 0;
-            pre = --target;
-            System.out.println(pre);
-            System.out.println(target);
+                String match1 = "ab*93";
+                String target1 = "a93";
+                boolean result1 = regMatch(target1.split(""),0,match1.split(""),0);
+                System.out.println(result1);
         }
 
 }
